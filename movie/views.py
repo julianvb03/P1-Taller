8d726a9825ce1acad141a88a3e17cec38ed6d2df
julian <valenciajuliann@hotmail.com>
2024-07-30 20:00:07 -0500

    visualizacion de películas con estilo

--- movie/views.py
+++ movie/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Movie
 
 # Create your views here.
 
 def home(request):
     #return HttpResponse('<h1>Welcome to Home Page</h1>')
     #return render(request, 'home.html')
-    context = {'name': 'Julian Valencia'}
+    searchTerm = request.GET.get('searchMovie')
+    if searchTerm == None:
+        movies = Movie.objects.all()
+    else:
+        movies = Movie.objects.filter(title__contains=searchTerm)
+    context = {'searchTerm': searchTerm, 'movies': movies}
     return render(request, 'home.html', context)
 
 def about(request):
